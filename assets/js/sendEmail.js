c156a87987fb8fd94f075d770bef925692587244
jfpaliga <>
2024-01-13 16:27:22 +0000

    Implement emailjs integration on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_vq75h1g", "resume_test", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;
+}
